8a86b862b1bd88ddb348d085c98a7e7d17d3679f
이현주 <hyunmail521@gmail.com>
2021-08-01 14:32:37 +0900

    delet: _app.tsx에서 common 스타일파일 제거

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app'
 import { GlobalStyles } from '../styles/global-styles'
-import '../styles/common'
 
 function App({ Component, pageProps }: AppProps) {
   return (
