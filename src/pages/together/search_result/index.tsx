3a74f33416d17adc33c6f0b73e0c552d93f47bf3
이현주 <hyunmail521@gmail.com>
2021-08-21 04:11:49 +0900

    add : 검색 결과 페이지

--- src/pages/together/search_result/index.tsx
+++ src/pages/together/search_result/index.tsx
@@ -1,5 +1,31 @@
-const SearchResult = () => {
-  return <></>
+import TogetherSearchBar from 'components/Together/Header/Search'
+import SearchBox from 'components/Together/SearchBox'
+import { searchWord } from './style'
+
+const Search = () => {
+  const word = '자바스크립트'
+
+  return (
+    <>
+      <TogetherSearchBar />
+
+      <div css={searchWord}>
+        <h2>
+          <strong>{word}</strong>
+          를(을) 검색한 결과입니다.
+        </h2>
+      </div>
+
+      <section>
+        <SearchBox listTitle="[서울] 취업용 프로젝트 같이하실 분  모집합니다." hashTag="Spring" />
+        <SearchBox listTitle="[서울] 취업용 프로젝트 같이하실 분  모집합니다." hashTag="Javascript" />
+        <SearchBox listTitle="[서울] 취업용 프로젝트 같이하실 분  모집합니다." hashTag="Spring" />
+        <SearchBox listTitle="[서울] 취업용 프로젝트 같이하실 분  모집합니다." hashTag="Javascript" />
+        <SearchBox listTitle="[서울] 취업용 프로젝트 같이하실 분  모집합니다." hashTag="Spring" />
+        <SearchBox listTitle="[서울] 취업용 프로젝트 같이하실 분  모집합니다." hashTag="Javascript" />
+      </section>
+    </>
+  )
 }
 
-export default SearchResult
+export default Search
--- src/pages/together/search_result/style.tsx
+++ src/pages/together/search_result/style.tsx
@@ -1,5 +1,17 @@
 import { css } from '@emotion/react'
 
-export const searchResult = css`
-  margin-top: 11em;
+export const searchWord = css`
+  font-weight: 500;
+  line-height: 24px;
+  letter-spacing: -1px;
+  font-size: 1.6rem;
+  color: #262626;
+  margin-top: 34px;
+  h2 {
+    margin-bottom: 20px;
+  }
+  strong {
+    font-size: 2rem;
+    font-weight: 700;
+  }
 `
