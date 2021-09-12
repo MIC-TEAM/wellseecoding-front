2de86ba42fdb979a76e793cf9ecc22333f95b0c2
이현주 <hyunmail521@gmail.com>
2021-09-12 19:28:52 +0900

    fix : 비밀번호 조건 수정

--- src/pages/sign_up/index.tsx
+++ src/pages/sign_up/index.tsx
@@ -121,7 +121,7 @@ const SingUp = () => {
         <div className="formbox">
           <PasswordField
             onChange={onChangePassword}
-            passwordText="비밀번호 (특수문자 포함 8자 이상)"
+            passwordText="비밀번호 (숫자+영문자+특수문자 조합으로 8자리 이상)"
             title="비밀번호"
             typeTitle="password"
           />
