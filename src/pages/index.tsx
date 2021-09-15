06efdd1211b66637635ede3f1234a914b0c0cf79
이준희 <junh0328@naver.com>
2021-09-15 20:14:13 +0900

    [#22] fix: 발급된 토큰 복호화 및 저장 마무리

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,24 +4,45 @@ import SplashScreen from 'components/SplashScreen'
 import { useEffect, useState } from 'react'
 
 function Home() {
-  const [tokenId, setTokenId] = useState()
+  const [tokenId, setTokenId] = useState(null)
+  const [decodedUserId, setDecodedUserId] = useState(null)
 
+  // ① 환경 변수에 등록한 토큰을 디코딩
   useEffect(() => {
     parseJwt(process.env.NEXT_PUBLIC_TOKEN)
   }, [])
 
+  // ② 객체에 담긴 복호화된 token 정보 중 userId를 추출
   useEffect(() => {
-    console.log('tokenId:', tokenId)
+    if (tokenId) checkId(tokenId)
   }, [tokenId])
 
+  // ③ 마지막 복호화된 토큰의 userId 확인
+  useEffect(() => {
+    if (decodedUserId) console.log('decodedUserId: ', decodedUserId)
+  }, [decodedUserId])
+
+  /* JWT 토큰을 디코딩(복호화)한다. */
   const parseJwt = (token: any) => {
     try {
-      // console.log(JSON.parse(atob(token.split('.')[1])))
+      console.log('start!', JSON.parse(atob(token.split('.')[1])))
       setTokenId(JSON.parse(atob(token.split('.')[1])))
     } catch (e) {
       return null
     }
   }
+  /* 복호화된 토큰 중 userId 정보를 분리한다. */
+  const checkId = (tokenId: any) => {
+    // 객체를 순회할 때는 for in문을 사용한다.
+    for (const key in tokenId) {
+      // console.log(key, tokenId[key])
+      if (key === 'sub') {
+        // console.log('tokenId의 키:', tokenId[key])
+        setDecodedUserId(tokenId[key])
+      }
+    }
+  }
+
   return (
     <>
       <div css={container}>
