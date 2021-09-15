c903472e1d1eb24d9f9d4d3d0a5cc83108ee6c6b
이준희 <junh0328@naver.com>
2021-09-15 19:50:52 +0900

    [#22] feat: 발급된 토큰 복호화 및 저장

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,27 @@
 import { css } from '@emotion/react'
 import FooterMenu from 'components/Common/FooterMenu'
 import SplashScreen from 'components/SplashScreen'
+import { useEffect, useState } from 'react'
 
 function Home() {
+  const [tokenId, setTokenId] = useState()
+
+  useEffect(() => {
+    parseJwt(process.env.NEXT_PUBLIC_TOKEN)
+  }, [])
+
+  useEffect(() => {
+    console.log('tokenId:', tokenId)
+  }, [tokenId])
+
+  const parseJwt = (token: any) => {
+    try {
+      // console.log(JSON.parse(atob(token.split('.')[1])))
+      setTokenId(JSON.parse(atob(token.split('.')[1])))
+    } catch (e) {
+      return null
+    }
+  }
   return (
     <>
       <div css={container}>
