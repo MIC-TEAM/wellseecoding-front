78c419aa7137559f49b73a279fc7bb782d13ed54
이현주 <hyunmail521@gmail.com>
2021-09-18 22:26:37 +0900

    fix : 학교 정보 api import

--- src/pages/sign_up/school/index.tsx
+++ src/pages/sign_up/school/index.tsx
@@ -1,36 +1,78 @@
 import FootButton, { FootButtonType } from 'components/Common/FootButton'
 import Back from 'components/Common/Header/Back'
 import Title from 'components/Common/Title'
-import TextField from 'components/Common/TextField'
+import TextFieldProfile from 'components/Common/TextFieldProfile'
 import Radio from '@material-ui/core/Radio'
 import RadioGroup from '@material-ui/core/RadioGroup'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import { css } from '@emotion/react'
+import { useRouter } from 'next/router'
+import { useState, useCallback } from 'react'
+import axios from 'axios'
+import { REGISTER_EDUCATION_URL } from 'apis'
 
 const SelfIntroduction = () => {
+  const [degree, setDegree] = useState<string>('')
+  const [major, setMajor] = useState<string>('')
+  const [graduated, setGraduated] = useState<boolean>(false)
+  const [disabled, setDisabled] = useState(false)
+  const router = useRouter()
+
+  const onSubmit = useCallback(async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    try {
+      await axios
+        .put(REGISTER_EDUCATION_URL, {
+          degree: degree,
+          major: major,
+          graduated: graduated,
+        })
+        .then((res) => {
+          console.log(res.data)
+        })
+    } catch (err) {
+      console.error(err)
+    }
+    setGraduated(true)
+    setDisabled(true)
+    router.push('/sign_up/experience')
+  }, [])
+
+  const onChangeDegree = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setDegree(e.target.value)
+    console.log(e.target.value)
+  }, [])
+
+  const onChangeMajor = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setMajor(e.target.value)
+    console.log(e.target.value)
+  }, [])
+
   return (
     <>
       <Back />
 
       <Title title="학교 정보를 적어주세요!" className="loginMt" />
 
-      <form css={selfWrap}>
-        <TextField text="학위" type="text" />
-        <TextField text="전공" type="text" />
+      <form css={selfWrap} onSubmit={onSubmit}>
+        <TextFieldProfile text="학위" type="text" onChange={onChangeDegree} />
+        <TextFieldProfile text="전공" type="text" onChange={onChangeMajor} />
 
         <RadioGroup row aria-label="school" name="school" defaultValue="top" style={{ marginTop: '2em' }}>
           <FormControlLabel value="school-ing" control={<Radio color="primary" />} label="재학 중" />
           <FormControlLabel value="school-end" control={<Radio color="primary" />} label="졸업" />
         </RadioGroup>
+
+        <div css={footButtonWrapper}>
+          <FootButton type="button" footButtonType={FootButtonType.SKIP}>
+            나중에 쓸게요~
+          </FootButton>
+          <FootButton type="button" footButtonType={FootButtonType.ACTIVATION} disabled={!disabled}>
+            다음
+          </FootButton>
+        </div>
       </form>
-      <div css={footButtonWrapper}>
-        <FootButton type="button" footButtonType={FootButtonType.SKIP}>
-          나중에 쓸게요~
-        </FootButton>
-        <FootButton type="button" footButtonType={FootButtonType.ACTIVATION}>
-          다음
-        </FootButton>
-      </div>
     </>
   )
 }
