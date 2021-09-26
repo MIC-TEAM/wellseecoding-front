4f8ab613a8fc665cbc4493ed4d6db14199e2b3e2
이현주 <hyunmail521@gmail.com>
2021-09-27 00:26:08 +0900

    add : email login working

--- src/pages/sign_in/email_start/index.tsx
+++ src/pages/sign_in/email_start/index.tsx
@@ -8,6 +8,8 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import FootButton, { FootButtonType } from 'components/Common/FootButton'
 import Back from 'components/Common/Header/Back'
+// import { REGISTER_USERS_URL } from 'apis'
+// import axios from 'axios'
 
 const EmailLogin = () => {
   const router = useRouter()
@@ -16,19 +18,48 @@ const EmailLogin = () => {
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
 
+  //오류메시지 상태저장
+  const [emailMessage, setEmailMessage] = useState<string>('')
+  const [passwordMessage, setPasswordMessage] = useState<string>('')
+
+  // 유효성 검사
+  const [isEmail, setIsEmail] = useState<boolean>(false)
+  const [isPassword, setIsPassword] = useState<boolean>(false)
+
   const onSubmit = useCallback(
     async (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault()
-      console.log(`이메일 + ${email}, 패스워드 + ${password}`)
       router.push('/home')
+      // try {
+      //   await axios
+      //     .post(REGISTER_USERS_URL, {
+      //       password: password,
+      //       email: email,
+      //     })
+      //     .then((res) => {
+      //       console.log(res.data)
+      //     })
+      // } catch (err) {
+      //   console.error(err)
+      // }
     },
     [email, password]
   )
 
   // 이메일
   const onChangeEmail = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const emailRegex =
+      /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
     const emailCurrent = e.target.value
     setEmail(emailCurrent)
+
+    if (!emailRegex.test(emailCurrent)) {
+      setEmailMessage('이메일 형식이 틀렸어요! 다시 확인해주세요 ㅜ ㅜ')
+      setIsEmail(false)
+    } else {
+      setEmailMessage('올바른 이메일 형식이에요 : )')
+      setIsEmail(true)
+    }
   }, [])
 
   // 비밀번호
@@ -38,9 +69,11 @@ const EmailLogin = () => {
     setPassword(passwordCurrent)
 
     if (!passwordRegex.test(passwordCurrent)) {
-      alert('비밀번호는 숫자 + 영문자 + 특수문자 조합으로 8자리 이상 입력해주세요.')
+      setPasswordMessage('숫자+영문자+특수문자 조합으로 8자리 이상 입력해주세요!')
+      setIsPassword(false)
     } else {
-      alert('비밀번호는 숫자 + 영문자 + 특수문자 조합으로 8자리 이상 입력해주세요.')
+      setPasswordMessage('안전한 비밀번호에요 : )')
+      setIsPassword(true)
     }
   }, [])
 
@@ -53,16 +86,24 @@ const EmailLogin = () => {
       </div>
 
       <form css={loginFrom} onSubmit={onSubmit}>
-        <TextField text="이메일" type="email" typeName="email" onChange={onChangeEmail} />
-        <PasswordField
-          title="비밀번호"
-          onChange={onChangePassword}
-          typeTitle="password"
-          passwordText="비밀번호 (숫자+영문자+특수문자 조합으로 8자리 이상)"
-        />
+        <div className="formbox">
+          <TextField text="이메일" type="email" typeName="email" onChange={onChangeEmail} />
+          {email.length > 0 && <span className={`message ${isEmail ? 'success' : 'error'}`}>{emailMessage}</span>}
+        </div>
 
+        <div className="formbox">
+          <PasswordField
+            onChange={onChangePassword}
+            passwordText="비밀번호 (숫자+영문자+특수문자 조합으로 8자리 이상)"
+            title="비밀번호"
+            typeTitle="password"
+          />
+          {password.length > 0 && (
+            <span className={`message ${isPassword ? 'success' : 'error'}`}>{passwordMessage}</span>
+          )}
+        </div>
         <div css={footButtonWrapper}>
-          <FootButton type="submit" footButtonType={FootButtonType.DISABLE}>
+          <FootButton type="submit" footButtonType={FootButtonType.ACTIVATION} disabled={!(isEmail && isPassword)}>
             다음
           </FootButton>
         </div>
@@ -83,6 +124,25 @@ export default EmailLogin
 const loginFrom = css`
   margin-top: 1.7em;
   padding: 0 20px;
+  .formbox {
+    position: relative;
+    margin-bottom: 20px;
+    .message {
+      font-weight: 500;
+      font-size: 1.6rem;
+      line-height: 24px;
+      letter-spacing: -1px;
+      position: absolute;
+      bottom: -10px;
+      left: 0;
+      &.success {
+        color: #8f8c8b;
+      }
+      &.error {
+        color: #ff2727;
+      }
+    }
+  }
 `
 
 const loginTitleWrap = css`
@@ -92,6 +152,11 @@ const loginTitleWrap = css`
 
 const footButtonWrapper = css`
   margin-top: 68px;
+  button:disabled,
+  button[disabled] {
+    background-color: #d3cfcc;
+    color: #ffffff;
+  }
 `
 
 const passwordFind = css`
