99bbc67aa975a0b0bbd1e22bbe3aa52355742ee3
이현주 <hyunmail521@gmail.com>
2021-10-27 22:29:22 +0900

    [#26] fix : wrap 테두리 수정

--- src/components/Common/Title/index.tsx
+++ src/components/Common/Title/index.tsx
@@ -18,7 +18,6 @@ const titleStyle = css`
   font-size: ${Common.fontSize.title};
   font-weight: 500;
   color: ${Common.colors.black};
-  margin-top: 2.3em;
   padding-left: 20px;
   .loginMt {
     margin-top: 3.7em;
